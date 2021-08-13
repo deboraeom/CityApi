4d0030513d290eae8487e9ab9bca184a9bba3ace
Débora Ester Oliveira Magalhães <deom3@hotmail.com>
2021-08-13 09:03:31 -0300

    Deletando projeto duplicado

--- src/main/java/com/example/cityapi/Country/CountryRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.cityapi.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long>{
-}
--- src/main/java/com/example/cityapi/state/StateRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.cityapi.state;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
